e2a89e1bd88e98feeabf55e735bf2b9b69654876
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-10-31 11:19:42 +0100

    added fonts and main layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/media.css') }}">
+
+    <title>@yield('page-title')</title>
+
+</head>
+<body>
+
+@yield('header')
+
+<main class="main">
+    @yield('content')
+</main>
+
+@yield('footer')
+
+<script defer src="{{ asset('js/main.js') }}"></script>
+@yield('scripts')
+</body>
+</html>
