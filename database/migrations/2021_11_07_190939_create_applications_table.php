47a2896e41d9f41cc4914bd61e524040d8484de2
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-11-07 22:18:17 +0100

    added Application model, migration and relationship

---
+++ database/migrations/2021_11_07_190939_create_applications_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->enum('gender', ['female', 'male']);
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('service_id');
+            $table->decimal('start_price')->nullable();
+            $table->decimal('end_price')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamp('start_date')->nullable();
+            $table->timestamp('end_date')->nullable();
+            $table->set('place', ['master', 'client', 'both'])->nullable();
+            $table->string('country')->nullable();
+            $table->string('region')->nullable();
+            $table->string('district')->nullable();
+            $table->string('city')->nullable();
+            $table->string('coord_lat')->nullable();
+            $table->string('coord_long')->nullable();
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('executor_id')->nullable();
+            $table->unsignedTinyInteger('is_closed')->default(0);
+            $table->timestamp('will_close_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
