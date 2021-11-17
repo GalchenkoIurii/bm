bb9fc5963dabbe91dfbe590efb9b300d0b0cc379
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-11-17 16:59:03 +0100

    added Categories index action and template

---
+++ resources/views/admin/categories.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.layouts.main')
+
+@section('content')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Категории</h1>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <h3>Управление категориями</h3>
+        </div>
+        <div class="card-body table-responsive">
+            <a class="btn btn-primary mb-2" href="{{ route('admin.categories.create') }}" role="button">Добавить категорию</a>
+            @if($categories->isNotEmpty())
+                <table class="table table-striped table-hover">
+                    <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <th scope="col">Название</th>
+                        <th scope="col">Слаг</th>
+                        <th scope="col">Лого</th>
+                        <th scope="col">Действия</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($categories as $category)
+                        <tr>
+                            <th scope="row">{{ $category->id }}</th>
+                            <td>{{ $category->name }}</td>
+                            <td>{{ $category->slug }}</td>
+                            <td>{{ $category->logo }}</td>
+                            <td class="d-flex">
+                                <a href="{{ route('admin.categories.edit', ['category' => $category->id]) }}"
+                                   class="btn btn-info btn-sm me-1">Редактировать</a>
+                                <form action="{{ route('admin.categories.destroy', ['category' => $category->id]) }}" method="post" class="">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm"
+                                            onclick="return confirm('Подтвердите удаление')">Удалить</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            @else
+                <div>Данных нет...</div>
+            @endif
+        </div>
+    </div>
+@endsection
--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -91,7 +91,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.index') }}">
+                        <a class="nav-link" href="{{ route('admin.categories.index') }}">
                             <span data-feather="sliders"></span>
                             Категории
                         </a>
