04aa20648b289e3cad759b59df1635dc2039d45a
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-12-07 23:37:00 +0100

    added show action and template for Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Profile;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $profile = Profile::with(['user'])->findOrFail($id);
+
+        return view('profiles.profiles-show', compact($profile));
+    }
 }
