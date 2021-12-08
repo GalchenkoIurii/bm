1df06636d4a77a6642dc8362c053dea3d99b1d0f
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-12-08 22:32:59 +0100

    added styles for Profile show

--- resources/views/profiles/profiles-show.blade.php
+++ resources/views/profiles/profiles-show.blade.php
@@ -11,34 +11,97 @@
         <div class="container">
             <h1 class="page-header">{{ $profile->user->first_name }} {{ $profile->user->last_name }}</h1>
 
-            @if($profile->user->is_master)
-                <p class="page-description">
-                    Email: {{ $profile->user->email }} <br>
-                    Номер телефона: {{ $profile->user->phone }} <br>
-                    Аватар: {{ $profile->avatar }} <br>
-                    Страна: {{ $profile->country }} <br>
-                    Область/Регион: {{ $profile->region }} <br>
-                    Район: {{ $profile->district }} <br>
-                    Город: {{ $profile->city }} <br>
-                    Улица: {{ $profile->street }} <br>
-                    Номер дома: {{ $profile->house }} <br>
-                    Номер помещения/квартиры: {{ $profile->locale_num }} <br>
-                    Место: {{ $profile->place }} <br>
-                    Обо мне: {{ $profile->about }} <br>
-                    Образование: {{ $profile->education }} <br>
-                    Опыт: {{ $profile->experience }} <br>
-                </p>
-            @else
-                <p class="page-description">
-                    Email: {{ $profile->user->email }} <br>
-                    Номер телефона: {{ $profile->user->phone }} <br>
-                    Аватар: {{ $profile->avatar }} <br>
-                    Страна: {{ $profile->user->country }} <br>
-                    Область/Регион: {{ $profile->user->region }} <br>
-                    Район: {{ $profile->user->district }} <br>
-                    Город: {{ $profile->user->city }} <br>
-                </p>
-            @endif
+            <div class="card-box">
+                <div class="card">
+                    @if($profile->user->is_master)
+                        <p class="card__item">
+                            <span class="card__item-title">Email: </span>
+                            <span class="card__item-content">{{ $profile->user->email }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Номер телефона: </span>
+                            <span class="card__item-content">{{ $profile->user->phone }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Аватар: </span>
+                            <span class="card__item-content">{{ $profile->avatar }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Страна: </span>
+                            <span class="card__item-content">{{ $profile->country }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Область/Регион: </span>
+                            <span class="card__item-content">{{ $profile->region }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Район: </span>
+                            <span class="card__item-content">{{ $profile->district }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Город: </span>
+                            <span class="card__item-content">{{ $profile->city }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Улица: </span>
+                            <span class="card__item-content">{{ $profile->street }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Номер дома: </span>
+                            <span class="card__item-content">{{ $profile->house }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Номер помещения/квартиры: </span>
+                            <span class="card__item-content">{{ $profile->locale_num }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Место: </span>
+                            <span class="card__item-content">{{ $profile->place }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Обо мне: </span>
+                            <span class="card__item-content">{{ $profile->about }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Образование: </span>
+                            <span class="card__item-content">{{ $profile->education }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Опыт: </span>
+                            <span class="card__item-content">{{ $profile->experience }}</span>
+                        </p>
+                    @else
+                        <p class="card__item">
+                            <span class="card__item-title">Email: </span>
+                            <span class="card__item-content">{{ $profile->user->email }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Номер телефона: </span>
+                            <span class="card__item-content">{{ $profile->user->phone }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Аватар: </span>
+                            <span class="card__item-content">{{ $profile->avatar }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Страна: </span>
+                            <span class="card__item-content">{{ $profile->user->country }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Область/Регион: </span>
+                            <span class="card__item-content">{{ $profile->user->region }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Район: </span>
+                            <span class="card__item-content">{{ $profile->user->district }}</span>
+                        </p>
+                        <p class="card__item">
+                            <span class="card__item-title">Город: </span>
+                            <span class="card__item-content">{{ $profile->user->city }}</span>
+                        </p>
+                    @endif
+                </div>
+            </div>
 
         </div>
     </section>
