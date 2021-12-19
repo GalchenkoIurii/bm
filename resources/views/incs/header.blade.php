5a337f63382ed9e9b98ef3a6fd93f96267f996e1
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-12-19 12:26:58 +0100

    added main styles for nav menu

--- resources/views/incs/header.blade.php
+++ resources/views/incs/header.blade.php
@@ -9,21 +9,21 @@
             <div class="menu">
                 <button id="mobileMenuOpen" class="menu__btn"></button>
                 <nav class="menu__nav">
-                    <ul class="">
-                        <li><a href="{{ route('home') }}">Главная</a></li>
-                        <li><a href="{{ route('search') }}">Найти мастера</a></li>
-                        <li><a href="{{ route('applications.create') }}">Оставить заявку</a></li>
-                        <li><a href="{{ route('about') }}">О сервисе</a></li>
+                    <ul class="menu-box">
+                        <li class="menu__item"><a class="menu__link" href="{{ route('home') }}">Главная</a></li>
+                        <li class="menu__item"><a class="menu__link" href="{{ route('search') }}">Найти мастера</a></li>
+                        <li class="menu__item"><a class="menu__link" href="{{ route('applications.create') }}">Оставить заявку</a></li>
+                        <li class="menu__item"><a class="menu__link" href="{{ route('about') }}">О сервисе</a></li>
                         @if(auth()->check() && auth()->user()->is_admin)
-                                <li><a href="{{ route('admin.index') }}">Админпанель</a></li>
-                                <li><a href="{{ route('profiles.show', ['profile' => $user_data['profile_id']]) }}">Мой профиль</a></li>
-                                <li><a href="{{ route('logout') }}">Выйти</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('admin.index') }}">Админпанель</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('profiles.show', ['profile' => $user_data['profile_id']]) }}">Мой профиль</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('logout') }}">Выйти</a></li>
                             @elseif(auth()->check())
-                                <li><a href="{{ route('profiles.show', ['profile' => $user_data['profile_id']]) }}">Мой профиль</a></li>
-                                <li><a href="{{ route('logout') }}">Выйти</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('profiles.show', ['profile' => $user_data['profile_id']]) }}">Мой профиль</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('logout') }}">Выйти</a></li>
                             @else
-                                <li><a href="{{ route('register.create') }}">Регистрация</a></li>
-                                <li><a href="{{ route('login.create') }}">Вход</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('register.create') }}">Регистрация</a></li>
+                                <li class="menu__item"><a class="menu__link" href="{{ route('login.create') }}">Вход</a></li>
                             @endif
                     </ul>
                 </nav>
