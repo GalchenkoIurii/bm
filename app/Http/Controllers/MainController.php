6d17a4efc4472da232f21283f3973d252c846db8
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-12-22 20:02:15 +0100

    added pagination nav styles for 1024-1280px width

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -38,7 +38,7 @@ class MainController extends Controller
 
     public function searchMasters($service)
     {
-        $users = User::with('profile')->whereRelation('services', 'slug', $service)->paginate(4);
+        $users = User::with('profile')->whereRelation('services', 'slug', $service)->paginate(1);
 
         return view('masters.masters-show', compact('users'));
     }
