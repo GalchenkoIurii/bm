a5c1bb638debd0390ace4fd8b710715479104b5a
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2021-12-27 19:25:21 +0100

    added box-sizing global style

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -38,7 +38,7 @@ class MainController extends Controller
 
     public function searchMasters($service)
     {
-        $users = User::with('profile')->whereRelation('services', 'slug', $service)->paginate(7);
+        $users = User::with('profile')->whereRelation('services', 'slug', $service)->paginate(9);
 
         return view('masters.masters-show', compact('users'));
     }
