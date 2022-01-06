fa8b9d0f5daadaeaad9e2d133f4df71a8255dcd7
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-01-06 15:17:59 +0100

    added main styles and template for single application

--- resources/views/applications/applications-show.blade.php
+++ resources/views/applications/applications-show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.main')
 
+@section('styles')
+    <link href="{{ asset('fa-web/css/all.css') }}" rel="stylesheet">
+@endsection
+
 @section('page-title')Заявка@endsection
 
 @section('header')
@@ -11,17 +15,106 @@
         <div class="container">
             <h1 class="page-header">Заявка # {{ $application->id }}</h1>
 
-                <p class="page-description">
-                    Категория: {{ $application->category->name }} <br>
-                    Услуга: {{ $application->service->name }} <br>
-                    Цена: от {{ $application->start_price }} грн. до {{ $application->end_price }} грн. <br>
-                    Даты: от {{ $application->start_date }} до {{ $application->end_date }} <br>
-                    Место: {{ $application->place }} <br>
-                    Страна: {{ $application->country }} <br>
-                    Регион: {{ $application->region }} <br>
-                    Город: {{ $application->city }} <br>
-                    Пользователь: {{ $application->user->first_name }} <br>
-                </p>
+            @if(session()->has('error'))
+                <p class="error-message">{{ session('error') }}</p>
+            @endif
+            @if($errors->any())
+                @foreach($errors->all() as $error)
+                    <p class="error-message">{{ $error }}</p>
+                @endforeach
+            @endif
+            @if(session()->has('success'))
+                <p class="status-message">{{ session('success') }}</p>
+            @endif
+
+                <div class="card card_mb">
+                    <div class="card__item card__item_centered card__item_hr-bottom">
+                        <div class="avatar-box">
+                            <div class="avatar">
+                                <div class="avatar__square">
+                                    <img src="{{ asset('storage/' . $application->user->profile->avatar) }}" alt="" class="avatar__image">
+                                </div>
+                            </div>
+                            @if($application->user->isOnline())
+                                <p class="avatar-box__status status-online">online</p>
+                            @else
+                                <p class="avatar-box__status status-not-online">не в сети</p>
+                            @endif
+                        </div>
+                        <div class="name-box">
+                            <h1 class="page-header">{{ $application->user->first_name }} {{ $application->user->last_name }}</h1>
+                        </div>
+                    </div>
+                    <p class="card__item">
+                        <span class="card__item-title">Категория:</span>
+                        <span class="card__item-content">{{ $application->category->name }}</span>
+                    </p>
+                    <p class="card__item">
+                        <span class="card__item-title">Услуга:</span>
+                        <span class="card__item-content">{{ $application->service->name }}</span>
+                    </p>
+                    @if(
+                        !is_null($application->start_price)
+                        || !is_null($application->end_price)
+                    )
+                        <p class="card__item card__item_centered">
+                            <span class="card__item-label"><i class="fas fa-dollar-sign"></i></span>
+                            <span class="card__item-text">
+                                                от {{ $application->start_price }} до {{ $application->end_price }} грн.
+                                        </span>
+                        </p>
+                    @endif
+                    @if(
+                        !is_null($application->start_date)
+                        || !is_null($application->end_date)
+                    )
+                        <p class="card__item card__item_centered">
+                            <span class="card__item-label"><i class="fas fa-calendar-alt"></i></span>
+                            <span class="card__item-text">
+                                                с {{ date('d.m.Y', strtotime($application->start_date)) }} до {{ date('d.m.Y', strtotime($application->end_date)) }}
+                                        </span>
+                        </p>
+                    @endif
+                    @if(!is_null($application->place))
+                        <p class="card__item card__item_column card__item_hr-top">
+                            <span class="card__item-title">Где могу получить услугу:</span>
+                            @if($application->place == 'master')
+                                <span class="card__item-content">У мастера</span>
+                            @elseif($application->place == 'client')
+                                <span class="card__item-content">У себя</span>
+                            @elseif($application->place == 'both')
+                                <span class="card__item-content">У себя или у мастера</span>
+                            @endif
+                        </p>
+                    @endif
+                    @if(
+                        !is_null($application->country)
+                        || !is_null($application->region)
+                        || !is_null($application->district)
+                        || !is_null($application->city)
+                    )
+                        <p class="card__item card__item_centered">
+                            <span class="card__item-label"><i class="fas fa-map-marker-alt"></i></span>
+                            <span class="card__item-text">
+                                {{ $application->country }}
+                                {{ $application->region }}
+                                {{ $application->district }}
+                                {{ $application->city }}
+                            </span>
+                        </p>
+                    @endif
+                </div>
+            <details class="accordion accordion-box__item">
+                <summary class="accordion__title">
+                    <span>Контакты</span>
+                </summary>
+                <ul class="accordion__text">
+                    {{ $application->country }}
+                    {{ $application->region }}
+                    {{ $application->district }}
+                    <li class="accordion__item">{{ $application->city }}</li>
+                </ul>
+            </details>
         </div>
     </section>
 @endsection
