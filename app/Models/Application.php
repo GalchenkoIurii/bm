841f24a556eab1cb333da5c1602a7863ae9880d1
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-01-09 17:03:59 +0100

    added fillable fields to Offer model, added application and offers relationships to Offer and Application models

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -38,6 +38,11 @@ class Application extends Model
         return $this->belongsTo(Category::class);
     }
 
+    public function offers()
+    {
+        return $this->hasMany(Offer::class);
+    }
+
     public function service()
     {
         return $this->belongsTo(Service::class);
--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Offer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'comment',
+        'accepted',
+        'application_id'
+    ];
+
+    public function application()
+    {
+        return $this->belongsTo(Application::class);
+    }
 }
