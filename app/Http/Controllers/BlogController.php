910baa8c02539a2db5c7ca3de29772dfca121516
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-01-21 13:22:36 +0100

    added posts tags links in sidebar

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Models\PostCategory;
+use App\Models\PostTag;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
@@ -13,13 +14,17 @@ class BlogController extends Controller
         if($request->has('category')) {
             $postCategory = PostCategory::with(['posts'])->where('slug', $request->query('category'))->firstOrFail();
             $posts = $postCategory->posts()->paginate();
+        } elseif($request->has('tag')) {
+            $postTag = PostTag::with(['posts'])->where('slug', $request->query('tag'))->firstOrFail();
+            $posts = $postTag->posts()->paginate();
         } else {
             $posts = Post::with(['postCategory', 'postTags'])->paginate();
         }
 
         $categories = PostCategory::all();
+        $tags = PostTag::all();
 
-        return view('blog.posts', compact('posts', 'categories'));
+        return view('blog.posts', compact('posts', 'categories', 'tags'));
     }
 
     public function show($post)
--- resources/views/incs/sidebar.blade.php
+++ resources/views/incs/sidebar.blade.php
@@ -17,4 +17,22 @@
             <p class="sidebar-widget__content">Категорий пока нет...</p>
         @endif
     </details>
+    <details class="sidebar-widget">
+        <summary class="sidebar-widget__title">
+            <h3>Теги</h3>
+            <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" class="sidebar-widget__title-icon">
+                <path d="M18.71,8.21a1,1,0,0,0-1.42,0l-4.58,4.58a1,1,0,0,1-1.42,0L6.71,8.21a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41l4.59,4.59a3,3,0,0,0,4.24,0l4.59-4.59A1,1,0,0,0,18.71,8.21Z"/>
+            </svg>
+        </summary>
+        @if($tags->isNotEmpty())
+            <ul class="sidebar-widget__content">
+                <li class="sidebar-widget__item"><a href="{{ route('blog.index') }}">Все теги</a></li>
+                @foreach($tags as $tag)
+                    <li class="sidebar-widget__item"><a href="{{ route('blog.index', ['tag' => $tag->slug]) }}">{{ $tag->title }} ({{ $tag->posts->count() }})</a></li>
+                @endforeach
+            </ul>
+        @else
+            <p class="sidebar-widget__content">Тегов пока нет...</p>
+        @endif
+    </details>
 </aside>
