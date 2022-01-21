02187d13f5819b054a8670dfadf9b4a213602ef7
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2022-01-21 12:47:26 +0100

    added styles for sidebar widgets

--- resources/views/blog/posts.blade.php
+++ resources/views/blog/posts.blade.php
@@ -23,19 +23,6 @@
     </section>
     <section class="section">
         <div class="container">
-            {{--<aside class="sidebar">--}}
-                {{--<div class="sidebar-widget">--}}
-                    {{--<h3>Категории</h3>--}}
-                    {{--@if($categories->isNotEmpty())--}}
-                        {{--<a href="{{ route('blog.index') }}">Все категории</a>--}}
-                        {{--@foreach($categories as $category)--}}
-                            {{--<a href="{{ route('blog.index', ['category' => $category->slug]) }}">{{ $category->title }}</a>--}}
-                        {{--@endforeach--}}
-                    {{--@else--}}
-                        {{--<p>Категорий пока нет...</p>--}}
-                    {{--@endif--}}
-                {{--</div>--}}
-            {{--</aside>--}}
             @include('incs.sidebar')
             <div class="content">
                 <h1 class="page-header">Посты</h1>
